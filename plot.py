cbbe2be9c065950c076afda335450ba429264dfe
rkp8000 <rpang.contact@gmail.com>
2016-10-28 21:58:50 -0700

    Add function to get evenly spaced colors from a color map.

--- plot.py
+++ plot.py
@@ -1,5 +1,5 @@
 from __future__ import division, print_function
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
 import numpy as np
 
 
@@ -7,10 +7,22 @@ def set_fontsize(ax, fontsize):
     """Set fontsize of all axis text objects to specified value."""
 
     for txt in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
-                ax.get_xticklabels() + ax.get_yticklabels()):
+        ax.get_xticklabels() + ax.get_yticklabels()):
+
         txt.set_fontsize(fontsize)
 
     legend = ax.get_legend()
+
     if legend:
-        for txt in legend.get_texts():
-            txt.set_fontsize(fontsize)
+        for txt in legend.get_texts(): txt.set_fontsize(fontsize)
+
+
+def get_n_colors(n, colormap='rainbow'):
+    """
+    Return a list of colors equally spaced over a color map.
+    :param n: number of colors
+    :param colormap: colormap to use
+    :return: list of colors that can be passed directly to color argument of plotting
+    """
+
+    return getattr(cm, colormap)(np.linspace(0, 1, n))
